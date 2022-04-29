947277200b1483c2d0e819341db7fbace8e32906
Elias Naur <mail@eliasnaur.com>
2022-04-29 13:40:31 +0200

    truetype: add truetype test data

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/benoitkugler/textlayout-testdata
+
+go 1.17
---
+++ truetype/data.go
@@ -0,0 +1,177 @@
+package truetype
+
+import _ "embed"
+
+//go:embed ankr.bin
+var Ankr_bin []byte
+
+//go:embed 04B_30.ttf
+var ZeroFourB_30_ttf []byte
+
+//go:embed AccanthisADFStdNo2-Regular.otf
+var AccanthisADFStdNo2_Regular_otf []byte
+
+//go:embed AnjaliOldLipi-Regular.ttf
+var AnjaliOldLipi_Regular_ttf []byte
+
+//go:embed ankr.bin
+var ankr_bin []byte
+
+//go:embed "Bangla Sangam MN.ttc"
+var Bangla_Sangam_MN_ttc []byte
+
+//go:embed Castoro-Italic.ttf
+var Castoro_Italic_ttf []byte
+
+//go:embed Castoro-Regular.ttf
+var Castoro_Regular_ttf []byte
+
+//go:embed CFFTest.otf
+var CFFTest_otf []byte
+
+//go:embed chromacheck-svg.ttf
+var chromacheck_svg_ttf []byte
+
+//go:embed cmap2.bin
+var cmap2_bin []byte
+
+//go:embed cmap2_expected.ttx
+var cmap2_expected_ttx []byte
+
+//go:embed Comfortaa-i.ttf
+var Comfortaa_i_ttf []byte
+
+//go:embed Commissioner-VF.ttf
+var Commissioner_VF_ttf []byte
+
+//go:embed Courier.dfont
+var Courier_dfont []byte
+
+//go:embed data.go
+var data_go []byte
+
+//go:embed DejaVuSerif.ttf
+var DejaVuSerif_ttf []byte
+
+//go:embed DFONT.dfont
+var DFONT_dfont []byte
+
+//go:embed Estedad-VF.ttf
+var Estedad_VF_ttf []byte
+
+//go:embed FreeSerif.ttf
+var FreeSerif_ttf []byte
+
+//go:embed freetype_ref.py
+var freetype_ref_py []byte
+
+//go:embed Gacha_9.dfont
+var Gacha_9_dfont []byte
+
+//go:embed GDEFCaretList3.ttf
+var GDEFCaretList3_ttf []byte
+
+//go:embed Geneva.dfont
+var Geneva_dfont []byte
+
+//go:embed IBM3161-bitmap.otb
+var IBM3161_bitmap_otb []byte
+
+//go:embed kernSubtable0.bin
+var kernSubtable0_bin []byte
+
+//go:embed kerxSubtable6.bin
+var kerxSubtable6_bin []byte
+
+//go:embed LateefGR-Regular.ttf
+var LateefGR_Regular_ttf []byte
+
+//go:embed Mada-VF.ttf
+var Mada_VF_ttf []byte
+
+//go:embed mry_KacstQurn.ttf
+var mry_KacstQurn_ttf []byte
+
+//go:embed NotoColorEmoji.ttf
+var NotoColorEmoji_ttf []byte
+
+//go:embed NotoSansArabic.ttf
+var NotoSansArabic_ttf []byte
+
+//go:embed NotoSansCJK-Bold.ttc
+var NotoSansCJK_Bold_ttc []byte
+
+//go:embed NotoSerifCJK-Regular.ttc
+var NotoSerifCJK_Regular_ttc []byte
+
+//go:embed OldaniaADFStd-Bold.otf
+var OldaniaADFStd_Bold_otf []byte
+
+//go:embed open-sans-v15-latin-regular.woff
+var open_sans_v15_latin_regular_woff []byte
+
+//go:embed Raleway-v4020-Regular.otf
+var Raleway_v4020_Regular_otf []byte
+
+//go:embed Roboto-BoldItalic.ttf
+var Roboto_BoldItalic_ttf []byte
+
+//go:embed segments.ttf
+var segments_ttf []byte
+
+//go:embed SelawikVar.ttf
+var SelawikVar_ttf []byte
+
+//go:embed SourceSansVariable-Roman.anchor.ttf
+var SourceSansVariable_Roman_anchor_ttf []byte
+
+//go:embed SourceSansVariable-Roman.modcomp.ttf
+var SourceSansVariable_Roman_modcomp_ttf []byte
+
+//go:embed SourceSansVariable-Roman-nohvar-41,C1.ttf
+var SourceSansVariable_Roman_nohvar_41_C1_ttf []byte
+
+//go:embed STIX-BoldItalic.otf
+var STIX_BoldItalic_otf []byte
+
+//go:embed TestCFF2VF.otf
+var TestCFF2VF_otf []byte
+
+//go:embed ToyCBLC1.ttf
+var ToyCBLC1_ttf []byte
+
+//go:embed ToyCBLC2.ttf
+var ToyCBLC2_ttf []byte
+
+//go:embed ToyCMAP12.otf
+var ToyCMAP12_otf []byte
+
+//go:embed ToyCMAP14.otf
+var ToyCMAP14_otf []byte
+
+//go:embed ToyFeat.ttf
+var ToyFeat_ttf []byte
+
+//go:embed ToyGPOSCursive.ttf
+var ToyGPOSCursive_ttf []byte
+
+//go:embed ToyGSUBLigature.ttf
+var ToyGSUBLigature_ttf []byte
+
+//go:embed ToyIndicGSUB.ttf
+var ToyIndicGSUB_ttf []byte
+
+//go:embed ToyKern1.ttf
+var ToyKern1_ttf []byte
+
+//go:embed ToySbix.ttf
+var ToySbix_ttf []byte
+
+//go:embed ToyTrak.ttf
+var ToyTrak_ttf []byte
+
+//go:embed ToyTTC.ttc
+var ToyTTC_ttc []byte
+
+//go:embed ToyVar1.ttf
+var ToyVar1_ttf []byte
